f78ea29239ce7123c49f9768b3d7260d3b83adda
LytvyniukDima <dima.lytvyniuk@gmail.com>
2019-09-24 19:41:25 +0300

    Add base components for lab1

--- front_end/pzks/src/app/app-routing.module.ts
+++ front_end/pzks/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Lab1Component } from './components/lab1/lab1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Lab1Component},
+  { path: 'lab1', component: Lab1Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front_end/pzks/src/app/app.module.ts
+++ front_end/pzks/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Lab1Component } from './components/lab1/lab1.component';
+import { BackendClientService } from './services/backend-client.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Lab1Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    BackendClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
